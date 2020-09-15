03ac2ace408b3c3bd7aee33f20cc8ebf1c3186f7
Harish Raisinghani <harish@surreydigitalacademy.ca>
2020-09-15 20:48:29 +0000

    Update basic template

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def hello_world():
+    return 'Hello, World!'
 
 
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
